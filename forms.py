bb432f96c75e7e918e70fe5378a3f4e37f20d3f9
rhfiguer <rhfiguer@gmail.com>
2021-05-02 16:15:41 +0200

    Version Domingo

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
 
